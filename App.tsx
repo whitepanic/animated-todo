8ae0e6d8a362704ff486d40eb98c6969ea9eb3c6
whitepanic820728 <whitepanic820728@gmail.com>
2022-05-04 19:09:43 +0900

    Implemnet Hello World

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import AppContiner from './src/components/app-container';
+import Main from './src/screens/main'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContiner>
+      <Main />
+    </AppContiner>
   );
 }
 
